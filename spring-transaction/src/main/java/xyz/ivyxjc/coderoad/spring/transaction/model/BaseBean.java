68c21f32fe77ed3b41223e4cad1b6bfcd14ec24e
ivyxjc <ivyxjc1994@hotmail.com>
2019-05-08 09:04:18 +0800

    :sparkles: add spring transaction models

---
+++ spring-transaction/src/main/java/xyz/ivyxjc/coderoad/spring/transaction/model/BaseBean.java
@@ -0,0 +1,14 @@
+package xyz.ivyxjc.coderoad.spring.transaction.model;
+
+import java.time.LocalDateTime;
+
+class BaseBean {
+
+    private LocalDateTime createdAt;
+
+    private String createdBy;
+
+    private LocalDateTime updatedAt;
+
+    private String updatedBy;
+}
