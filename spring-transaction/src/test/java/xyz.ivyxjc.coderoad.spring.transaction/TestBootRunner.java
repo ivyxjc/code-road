d9f6630412ce28e0d5f90f77f3aee58028cf5e26
ivyxjc <ivyxjc1994@hotmail.com>
2019-05-11 18:29:19 +0800

    :construction:

---
+++ spring-transaction/src/test/java/xyz.ivyxjc.coderoad.spring.transaction/TestBootRunner.java
@@ -0,0 +1,13 @@
+package xyz.ivyxjc.coderoad.spring.transaction;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestBootRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootApplication.class);
+    }
+}
