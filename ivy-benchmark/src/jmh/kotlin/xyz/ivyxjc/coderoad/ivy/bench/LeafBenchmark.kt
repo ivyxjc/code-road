bb1daed9aeadb560729aa26e80e07a0ce2ec4387
ivyxjc <ivyxjc1994@hotmail.com>
2019-05-15 22:00:06 +0800

    :sparkles: snowflake generator v2

--- ivy-benchmark/src/jmh/kotlin/xyz/ivyxjc/coderoad/ivy/bench/LeafBenchmark.kt
+++ ivy-benchmark/src/jmh/kotlin/xyz/ivyxjc/coderoad/ivy/bench/LeafBenchmark.kt
@@ -1,35 +1,35 @@
 package xyz.ivyxjc.coderoad.ivy.bench
 
 import org.openjdk.jmh.annotations.*
-import org.openjdk.jmh.runner.Runner
-import org.openjdk.jmh.runner.options.OptionsBuilder
-import xyz.ivyxjc.coderoad.ivy.leaf.snowflake.SnowflakeIdGen
+import xyz.ivyxjc.coderoad.ivy.leaf.snowflake.SnowflakeIdGenV1
+import xyz.ivyxjc.coderoad.ivy.leaf.snowflake.SnowflakeIdGenV2
 import java.util.concurrent.TimeUnit
-
-
-fun main() {
-    val opt = OptionsBuilder()
-        .include(LeafBenchmarkRunner::class.java.simpleName)
-        .forks(1)
-        .warmupIterations(5)
-        .measurementIterations(5)
-        .threads(8)
-        .build()
-
-    Runner(opt).run()
-}
 
 @BenchmarkMode(Mode.All)
 @OutputTimeUnit(TimeUnit.NANOSECONDS)
 @State(Scope.Thread)
-open class LeafBenchmarkRunner {
-    val idGen = SnowflakeIdGen()
+open class SnoflakeIdGenV1Bench {
+    val idGen = SnowflakeIdGenV1()
 
     @Benchmark
+    @Synchronized
     fun genId() {
         idGen.get("")
     }
+}
 
+
+@BenchmarkMode(Mode.All)
+@OutputTimeUnit(TimeUnit.NANOSECONDS)
+@State(Scope.Thread)
+open class SnoflakeIdGenV2Bench {
+    val idGen = SnowflakeIdGenV2()
+
+    @Benchmark
+    @Synchronized
+    fun genId() {
+        idGen.get("")
+    }
 }
 
 
--- ivy-leaf/src/main/kotlin/xyz/ivyxjc/coderoad/ivy/leaf/snowflake/Snowflake.kt
+++ ivy-leaf/src/main/kotlin/xyz/ivyxjc/coderoad/ivy/leaf/snowflake/Snowflake.kt
@@ -1,10 +1,20 @@
 package xyz.ivyxjc.coderoad.ivy.leaf.snowflake
 
+import org.slf4j.LoggerFactory
 import xyz.ivyxjc.coderoad.ivy.leaf.IDGenerator
 import xyz.ivyxjc.coderoad.ivy.leaf.Result
 import xyz.ivyxjc.coderoad.ivy.leaf.Status
 
-class SnowflakeIdGen : IDGenerator {
+
+/**
+ *  A very simple id generator
+ *  Used as performance baseline
+ *  There are some bugs in this generator:
+ *      1. Cannot handle the condition of clock back
+ *      2. Generate duplicate ids when speed is too fast
+ *      ( not to check current time and last time)
+ */
+class SnowflakeIdGenV1 : IDGenerator {
 
     companion object {
         val workerIdLength: Int = 10
@@ -25,9 +35,85 @@ class SnowflakeIdGen : IDGenerator {
     }
 }
 
+
+/**
+ * one single-machine id generator
+ *
+ * If clock is taken back, the Result is with exception
+ */
+class SnowflakeIdGenV2 : IDGenerator {
+    private val log = LoggerFactory.getLogger(SnowflakeIdGenV2::class.java)
+
+    private var lastTimestamp: Long = System.currentTimeMillis()
+    private val lock = java.lang.Object()
+
+    companion object {
+        val workerIdLength: Int = 10
+        val serialLength: Int = 12
+        val wokerId = "0000000001".toLong(2)
+        val maxSerialNumber = 4095L
+        var serialNumber = 0L
+    }
+
+    override fun get(key: String): Result {
+        var status = Status.SUCCESS
+        var currentTime = timeGen()
+        if (currentTime < lastTimestamp) {
+            val offset = lastTimestamp - currentTime
+            if (offset < 5) {
+                try {
+                    lock.wait(5)
+                    currentTime = timeGen()
+                    if (currentTime < lastTimestamp) {
+                        return Result(-3, Status.EXCEPTION)
+                    }
+                } catch (e: InterruptedException) {
+                    log.error("wait interrupt", e)
+                    return Result(-1, Status.EXCEPTION)
+                }
+            } else {
+                return Result(-3, Status.EXCEPTION)
+            }
+        }
+
+        val timeInId = currentTime shl (workerIdLength + serialLength)
+        val workerIdInId = wokerId shl serialLength
+        /*
+         When time equals to the before one,
+         need to check whether serialNumber reaches the max number.
+         If yes, the program should wait until next millisecond coming
+         */
+        if (currentTime == lastTimestamp) {
+            if (serialNumber and maxSerialNumber == maxSerialNumber) {
+                serialNumber = 0
+                untilNextMillis()
+            }
+        } else {
+            serialNumber = 0
+        }
+        lastTimestamp = currentTime
+        val res = timeInId or workerIdInId or (serialNumber and maxSerialNumber)
+        serialNumber += 1
+        return Result(res, status)
+    }
+
+    fun untilNextMillis() {
+        var time = timeGen()
+        while (lastTimestamp == time) {
+            time = timeGen()
+        }
+    }
+
+    fun timeGen(): Long {
+        return System.currentTimeMillis();
+    }
+}
+
+
 fun main() {
     println(4095L.toString(2))
-    val idGenerator = SnowflakeIdGen()
+    println(4095L and 4095L == 4095L)
+    val idGenerator = SnowflakeIdGenV1()
     for (i in 0 until 100) {
         val res = idGenerator.get("hello")
         println(res.id.toString(2))
